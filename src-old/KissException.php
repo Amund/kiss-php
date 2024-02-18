c3887c8d0483e0a2dc825e720e8900c660a93260
Amund <mailamund@gmail.com>
2024-02-18 18:46:03 +0100

    prepare global rewrite

---
+++ src-old/KissException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kiss;
+
+class KissException extends \Exception
+{
+    public function __construct(
+        $message,
+        $code = 0,
+        \Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+}
