1dac99c523f259683d413bedc32946040c8959ee
Amund <mailamund@gmail.com>
2024-02-18 21:47:03 +0100

    exception: comments

--- src-old/KissException.php
+++ src-old/KissException.php
@@ -4,6 +4,14 @@ namespace Kiss;
 
 class KissException extends \Exception
 {
+    /**
+     * Throw a `KissException`.
+     *
+     * @param string $str The error message
+     * @param int $code The error code
+     * @param \Throwable|null $previous Previous exception
+     * @return void
+     */
     public function __construct(
         $message,
         $code = 0,
